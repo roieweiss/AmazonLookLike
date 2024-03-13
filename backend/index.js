88f66412698f4b88905f884d8977e583abf96208
roieweiss <roiemw@gmail.com>
2024-03-13 18:53:59 +0200

    added mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import User from "./models/User";
 
 const app = express();
 
@@ -9,7 +11,17 @@ app.use(express.urlencoded({extended: false}));
 // this threee lines is used in all projects
 
 //routs:
+//app.post('/addUser', async(req, res)=>{
+  //  const {user} = req.body;
+    //const newUser = await User.create(user);
+    //res.send(newUser);
+//})
 
+
+mongoose.connect("mongodb+srv://roiemw:devdev@cluster0.3igmy94.mongodb.net/AmazonLookalike?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
 app.listen(8080,function(){
  console.log("listening on" + 8080);
 });
+}).catch(err=> {console.log(err.massage);});
+
+
