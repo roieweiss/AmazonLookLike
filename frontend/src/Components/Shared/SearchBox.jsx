33c8b76ea35956c5a8d45dca5621b18a307115a1
roieweiss <roiemw@gmail.com>
2024-03-18 15:38:07 +0200

    change SearchBox

--- frontend/src/Components/Shared/SearchBox.jsx
+++ frontend/src/Components/Shared/SearchBox.jsx
@@ -4,17 +4,16 @@ import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
 
 const SearchBox = () => {
-  return (
-<Form>
-    <InputGroup>
-    <FormControl type="text" name="q" id="q" placeholder="search for product" aria-describedby="button-search">
-        <Button >
-            <i className="fa fa-search"></i>
-        </Button>
-    </FormControl>
-    </InputGroup>
-</Form>
-  )
+    return (
+        <Form>
+            <InputGroup>
+                <FormControl type="text" name="q" id="q" placeholder="search for product" aria-describedby="button-search"></FormControl>
+                <Button>
+                    <i className="fa fa-search"></i>
+                </Button>
+            </InputGroup>
+        </Form>
+    )
 }
 
 export default SearchBox
