127f7d01338700edf2c1b08b9c47ce35fcd34641
roieweiss <roiemw@gmail.com>
2024-03-18 14:24:43 +0200

    change header and download react-router-bootstrap

--- frontend/src/Components/Shared/Header.jsx
+++ frontend/src/Components/Shared/Header.jsx
@@ -1,9 +1,18 @@
-import {NavBar} from 'react-bootstrap/Navbar';
+import NavBar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import {LinkContainers} from 'react-router-bootstrap';
 
 const Header = () => {
   return (
     <header>
 <NavBar bg="dark" variant="dark">
+    <Container>
+        <LinkContainer>
+        <NavBar.Brand>
+            <img src="https://companieslogo.com/img/orig/AMZN_BIG.D-8fb0be81.png?t=1632523695" width={80} alt='Amazon logo'/>
+        </NavBar.Brand>
+        </LinkContainer>
+    </Container>
 
 </NavBar>
 
