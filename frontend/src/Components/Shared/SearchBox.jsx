584447facf2b832f7efc4b4031faa56cca7b6392
roieweiss <roiemw@gmail.com>
2024-03-18 15:36:03 +0200

    added Button to SearchBox

--- frontend/src/Components/Shared/SearchBox.jsx
+++ frontend/src/Components/Shared/SearchBox.jsx
@@ -1,12 +1,17 @@
 import Form from "react-bootstrap/Form"
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
 
 const SearchBox = () => {
   return (
 <Form>
     <InputGroup>
-    <FormControl type="text" name="q" id="q" placeholder="search for product" aria-describedby="button-search"></FormControl>
+    <FormControl type="text" name="q" id="q" placeholder="search for product" aria-describedby="button-search">
+        <Button >
+            <i className="fa fa-search"></i>
+        </Button>
+    </FormControl>
     </InputGroup>
 </Form>
   )
