f86ad2ed5547874a396f0dd2d264c52e88642ec2
roieweiss <roiemw@gmail.com>
2024-03-19 15:50:26 +0200

    change HomePage

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,24 +1,26 @@
 import { useEffect, useReducer } from "react";
 import Title from "../Components/Shared/Title";
 import homePageReducer from "../Reducers/homePageReducer";
-//import getProducts from "../../../backend/controllers/productsController";
 import axios from 'axios';
-import Loading from '../Components/Shared/Loading'
+import Loading from '../Components/Shared/Loading';
+import Product from "../../../backend/models/Product";
 
 
-const initialState = { loading: true, error: '', data: [] }
-const HomePage = () => {
-  const [state, dispatch] = useReducer(homePageReducer, initialState)
-  const {loading, error, data} = state;
+
+
+const initialState = { loading: true, error: "", data: [] };
+export const HomePage = () => {
+  const [{ loading, error, data }, dispatch] =
+    useReducer(homePageReducer, initialState);
   useEffect(() => {
     const getProducts = async () => {
       dispatch({ type: "GET_REQUEST" });
       try {
-        const { data } = await axios.get("http://localhost:8080/api/v1/products")
+        const { data } = await axios.get("/api/v1/products")
         dispatch({ type: "GET_SUCCESS", payload: data });
       } catch (error) {
-        dispatch({ type: "GET_FAIL", payload: error });
-        console.log(state.error.mwssage);
+        dispatch({ type: "GET_FAIL", payload: error.message });
+        console.log(error.message);
       }
     };
     getProducts();
@@ -26,17 +28,17 @@ const HomePage = () => {
   return (
     <div>
       <Title title="Amazon Shop"></Title>
-      <div className='backgroundHomePage'>
-        <img style={{ width: "100%" }} src="https://m.media-amazon.com/images/I/81d5OrWJAkL._SX3000_.jpg " alt="backgroundHomePage" />
+      <div className="backgroundHomePage">
+        <img style={{ width: "100%" }}
+          src="https://m.media-amazon.com/images/I/81d5OrWJAkL._SX3000_.jpg "
+          alt="backgroundHomePage" />
       </div>
       <div className="products">
-        {loading? <Loading/> : error? <MessageBox/> : (
-          <div> Display Products here</div>
-        ) }
+        {loading ? (<Loading />) : error ?
+          (<MessageBox variant="danger">{error}</MessageBox>)
+          : (<Products products={data}></Products>
+          )}
       </div>
-
     </div>
-  )
-}
-
-export default HomePage
+  );
+};
