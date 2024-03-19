4dcc3ed7efc62921185f87092c1c4a88c55245db
roieweiss <roiemw@gmail.com>
2024-03-19 14:11:00 +0200

    added reducer

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,12 +1,29 @@
+import { useEffect, useReducer } from "react";
 import Title from "../Components/Shared/Title";
+import homePageReducer from "../Reducers/homePageReducer";
+//import getProducts from "../../../backend/controllers/productsController";
+import axios from 'axios';
 
+const initialState = { loading: true, error: '', data: [] }
 const HomePage = () => {
+  const [state, dispatch] = useReducer(homePageReducer, initialState)
+  useEffect(() => {
+    const getProducts = async () => {
+      dispatch({ type: "GET_REQUEST" });
+      try {
+        const { data } = await axios.get("http://localhost:8080/api/v1/products")
+        dispatch({ type: "GET_SUCCESS", payload: data});
+      } catch (error) {
+        dispatch({ type: "GET_FAIL", payload: error});
+      }
+    };
+    getProducts();
+  }, [])
   return (
-    
     <div>
-        <Title title="Amazon Shop"></Title>
-        <h1> hello </h1>
-        </div>
+      <Title title="Amazon Shop"></Title>
+      <h1> hello </h1>
+    </div>
   )
 }
 
---
+++ frontend/src/Reducers/homePageReducer.jsx
@@ -0,0 +1,15 @@
+const homePageReducer = (state, action) => {
+switch (action.type) {
+    case 'GET_REQUSEST':{
+        return {...state, loading : true}
+    }
+    case 'GET_FAIL':{
+        return {...state, loading : false, error: action.payload};
+    }
+    case 'GET_SUCCESS':{
+        return {...state, loading : false, data: action.payload};
+    }
+    default: return {...state };
+}
+}
+export default homePageReducer;
