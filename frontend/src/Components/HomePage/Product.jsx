35f1496e4693167b7d15ba935b2005c536ec3264
roieweiss <roiemw@gmail.com>
2024-03-20 11:06:58 +0200

    did import in HomePage and Product

--- frontend/src/Components/HomePage/Product.jsx
+++ frontend/src/Components/HomePage/Product.jsx
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 
 const Product = ({product}) => {
@@ -12,7 +13,9 @@ const Product = ({product}) => {
         <Link to={`/product/${product.token}`}>
             <Card.Title>{product.title}</Card.Title>
         </Link>
-        
+        <Card.Text>${product.price}</Card.Text>
+        {product.countInStock===0? <Button variant="light" disabled>Out Of Stock</Button>:
+        <Button className="btn-primary" > Add to Cart</Button>}
     </Card.Body>
 </Card>
   )
--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -4,6 +4,7 @@ import homePageReducer from "../Reducers/homePageReducer";
 import axios from 'axios';
 import Loading from '../Components/Shared/Loading';
 import Product from "../../../backend/models/Product";
+import Products from "../Components/HomePage/Products";
 
 
 
@@ -20,8 +21,8 @@ export const HomePage = () => {
         const { data } = await axios.get("http://localhost:8080/api/v1/products")
         dispatch({ type: "GET_SUCCESS", payload: data });
       } catch (error) {
-        dispatch({ type: "GET_FAIL", payload: error });
-        console.log(state.error.message);
+        dispatch({ type: "GET_FAIL", payload: error.message });
+        console.log(error.message);
       }
     };
     getProducts();
@@ -35,8 +36,8 @@ export const HomePage = () => {
           alt="backgroundHomePage" />
       </div>
       <div className="products">
-        {loading ? <Loading /> : error ? <MessageBox></MessageBox> : (
-          <div> Display</div>
+        {loading ? <Loading /> : error ? <MessageBox variant="danger">{error}</MessageBox> : (
+          <Products products={data}></Products>
           )}
           </div>
     </div>
