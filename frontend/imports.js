633dec8b2245d173d0b5b3899a2cf07136e05c4b
roieweiss <roiemw@gmail.com>
2024-03-21 12:26:43 +0200

    imports to make things easier

---
+++ frontend/imports.js
@@ -0,0 +1,36 @@
+import {useState, useReducer} from "react";
+import axios from "axios";
+import Container from "react-bootstrap/Container";
+import PropTypes from "prop-types";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import NavBar from 'react-bootstrap/Navbar';
+import { LinkContainer } from 'react-router-bootstrap';
+import Spinner from "react-bootstrap/Spinner";
+import Alert from "react-bootstrap/Alert";
+import { Helmet } from "react-helmet-async";
+import Form from "react-bootstrap/Form"
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+
+export {useState,
+     useReducer, 
+     axios, 
+     Container, 
+     PropTypes, 
+     Card, 
+     Button, 
+     Link, 
+     Col, 
+     Row, 
+     NavBar,
+    LinkContainer,
+Spinner,
+Alert,
+Helmet,
+Form,
+InputGroup,
+FormControl}
--- frontend/src/Components/HomePage/Product.jsx
+++ frontend/src/Components/HomePage/Product.jsx
@@ -4,15 +4,37 @@ import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import Rating from "../Shared/Rating";
 
-const Product = ({product}) => {
+const Product = ({ product }) => {
   return (
     <Card className="product-card mb-4">
       <Link to={`/product/${product.token}`}>
-        <Card.img variant="top" src={product.image} alt={product.title}/>
+        <Card.img variant="top" src={product.image} alt={product.title} />
       </Link>
+      <Card.Body className="card-body">
+        <Link to={`/product/${product.token}`}>
+          <Card.Title>{product.title}</Card.Title>
+        </Link>
+        <Rating
+          rating={product.Rating.rate}
+          numReviews={product.Rating.count}
+        />
+        <Card.Text>${product.price}</Card.Text>
+        {product.countInStock === 0 ? (
+          <Button variant="light" disabled>
+            Out Of Stock
+          </Button>
+        ) : (
+          <Button className="btn-primary"
+            onClick={() => {
+              addToCartHandler(product, cartItems, ctxDispatch);
+            }}
+          >
+            Add To Cart
+          </Button>
+        )};
+      </Card.Body>
     </Card>
-
-  )
-}
+  );
+};
 Product.propTypes = { product: PropTypes.object };
 export default Product
