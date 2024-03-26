aa4bb5cca32758a39efd632dec67321a1392a7e0
roieweiss <roiemw@gmail.com>
2024-03-26 12:43:23 +0200

    change productRouter.js

--- backend/routes/productsRouter.js
+++ backend/routes/productsRouter.js
@@ -1,7 +1,19 @@
-import express  from "express";
-import getProducts from "../controllers/productsController.js";
+import expressAsyncHandler from "express-async-handler";
+import express from "express";
+import {
+  getProducts,
+  getProductById,
+  getProductByToken,
+  getCategories,
+  getProductsByQuery,
+} from "../controllers/productsController.js";
 
-const seedRouter = express.Router();
-seedRouter.get('/', getProducts());
+const productsRouter = express.Router();
+
+productsRouter.get("/", expressAsyncHandler(getProducts));
+productsRouter.get("/categories", expressAsyncHandler(getCategories));
+productsRouter.get("/search", expressAsyncHandler(getProductsByQuery));
+productsRouter.get("/token/:token", expressAsyncHandler(getProductByToken));
+productsRouter.get("/:id", expressAsyncHandler(getProductById));
 
 export default productsRouter;
