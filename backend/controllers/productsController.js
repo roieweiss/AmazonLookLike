3c63c602770466f263320d0de1101d63219e4401
roieweiss <roiemw@gmail.com>
2024-03-26 14:05:59 +0200

    added files

--- backend/controllers/productsController.js
+++ backend/controllers/productsController.js
@@ -1,10 +1,116 @@
-//import User from "../models/User.js";
 import Product from "../models/Product.js";
-//import data from "../data.js";
 
 const getProducts = async (req, res) => {
-    const products = await Product.find();
-    res.send(products);
-}
+  const products = await Product.find();
+  res.send(products);
+};
 
-export default getProducts;
+const getProductById = async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product not found" });
+  }
+};
+
+const getProductByToken = async (req, res) => {
+  const product = await Product.findOne({ token: req.params.token });
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product not found" });
+  }
+};
+
+const getCategories = async (req, res) => {
+  const categories = await Product.find().distinct("category");
+  res.send(categories);
+};
+
+const getProductsByQuery = async (req, res) => {
+  //Most complicated Function EVER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  const { query } = req;
+  const page = query.page || 1;
+  const order = query.order || "";
+  const category = query.category || "";
+  const price = query.price || "";
+  const rating = query.rating || "";
+  const searchQuery = query.query || "";
+  const pageSize = query.pageSize || 6;
+
+  const queryFilter =
+    searchQuery && searchQuery !== "all"
+      ? {
+          title: {
+            $regex: searchQuery,
+            $options: "i",
+          },
+        }
+      : {};
+  const categoryFilter =
+    category && category !== "all"
+      ? {
+          category,
+        }
+      : {};
+  const priceFilter =
+    price && price !== "all"
+      ? {
+          price: {
+            $gte: Number(price.split("-")[0]),
+            $lte: Number(price.split("-")[1]),
+          },
+        }
+      : {};
+  const ratingFilter =
+    rating && rating !== "all"
+      ? {
+          "rating.rate": {
+            $gte: Number(rating),
+          },
+        }
+      : {};
+  const sortOrderFilter =
+    order === "lowest"
+      ? { price: 1 }
+      : order === "highest"
+      ? { price: -1 }
+      : order === "toprated"
+      ? { rating: -1 }
+      : order === "newest"
+      ? { createdAt: -1 }
+      : { _id: -1 };
+
+  const products = await Product.find({
+    ...queryFilter,
+    ...categoryFilter,
+    ...priceFilter,
+    ...ratingFilter,
+  })
+    .sort(sortOrderFilter)
+    .skip((page - 1) * pageSize)
+    .limit(pageSize);
+
+  const countProducts = await Product.countDocuments({
+    ...queryFilter,
+    ...categoryFilter,
+    ...priceFilter,
+    ...ratingFilter,
+  });
+
+  res.send({
+    products,
+    countProducts,
+    page,
+    pages: Math.ceil(countProducts / pageSize),
+  });
+};
+
+export {
+  getProducts,
+  getProductById,
+  getProductByToken,
+  getCategories,
+  getProductsByQuery,
+};
